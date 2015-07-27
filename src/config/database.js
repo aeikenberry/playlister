172ec5188c2acb4f369e8bcb605803f778eaf1d0
Aaron Eikenberry <aeikenberry@gmail.com>
2015-07-26 23:16:07 -0500

    cool Typo

--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: process.env.MONGOLAB_URL || process.env.DATABASE_URL || 'mongodb://localhost'
+    url: process.env.MONGOLAB_URI || process.env.DATABASE_URL || 'mongodb://localhost'
 };
